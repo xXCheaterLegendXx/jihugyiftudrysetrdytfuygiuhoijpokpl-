29bea2885414818901d9d2844d2eca8ee81b8790
xXCheaterLegendXx <75645483+xXCheaterLegendXx@users.noreply.github.com>
2022-01-22 19:44:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0; 
+rightWristY = 0;
+
+function preload() {
+    song1 =  loadSound("Believer.mp3");
+    song2 =  loadSound("BDOML.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoSeNeT Is iNiTiAliZeD');
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function gotPoses(results) 
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX +" leftWristY ="+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX +" rightWristY ="+ rightWristY);
+    }
+}
